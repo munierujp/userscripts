b03a4b8a87f1c68e43c0a30e54788ff021cc504a
Munieru <20086673+munierujp@users.noreply.github.com>
2020-07-08 10:22:19 +0900

    Create show-account-name-on-header.user.js

---
+++ scripts/aws-management-console/show-account-name-on-header.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Show account name on header on AWS Management Console
+// @namespace    https://github.com/munierujp/
+// @version      0.1.0
+// @description  Show account name on header on AWS Management Console
+// @author       https://github.com/munierujp/
+// @homepageURL  https://github.com/munierujp/userscripts
+// @updateURL    https://raw.githubusercontent.com/munierujp/userscripts/master/scripts/aws-management-console/show-account-name-on-header.user.js
+// @downloadURL  https://raw.githubusercontent.com/munierujp/userscripts/master/scripts/aws-management-console/show-account-name-on-header.user.js
+// @supportURL   https://github.com/munierujp/userscripts/issues
+// @match        https://*.console.aws.amazon.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  const usernameMenuElement = document.getElementById('nav-usernameMenu')
+
+  if (!usernameMenuElement) {
+    return
+  }
+
+  const usernameMenuLabelElement = usernameMenuElement.querySelector('.nav-elt-label')
+
+  if (!usernameMenuLabelElement) {
+    return
+  }
+
+  const accountNameElement = document.getElementById('awsc-login-display-name-account')
+
+  if (!accountNameElement) {
+    return
+  }
+
+  const accountName = accountNameElement.textContent
+
+  if (!accountName) {
+    return
+  }
+
+  usernameMenuLabelElement.textContent = accountName
+})()
