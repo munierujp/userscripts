eeb11e5a885c4a4e002e1d141a1c6951fe55c1b9
Munieru <20086673+munierujp@users.noreply.github.com>
2020-07-30 00:00:12 +0900

    Create redirect-to-latest-page.user.js

---
+++ scripts/vuejs/redirect-to-latest-page.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Redirect to latest page on Vue.js  document
+// @namespace    https://github.com/munierujp/
+// @version      1.0.0
+// @description   Redirect to latest page on Vue.js  document
+// @author       https://github.com/munierujp/
+// @homepageURL  https://github.com/munierujp/userscripts
+// @updateURL    https://munierujp.github.io/userscripts/scripts/vuejs/redirect-to-latest-page.user.js
+// @downloadURL  https://munierujp.github.io/userscripts/scripts/vuejs/redirect-to-latest-page.user.js
+// @supportURL   https://github.com/munierujp/userscripts/issues
+// @match        https://011.vuejs.org/*
+// @match        https://012.vuejs.org/*
+// @match        https://012-cn.vuejs.org/*
+// @match        https://012-jp.vuejs.org/*
+// @match        https://v1.vuejs.org/*
+// @match        https://v1-cn.vuejs.org/*
+// @match        https://v1-jp.vuejs.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict'
+
+  const toLatestHost = (oldHost) => {
+    switch (oldHost) {
+      case '011.vuejs.org':
+      case '012.vuejs.org':
+      case 'v1.vuejs.org':
+        return 'vuejs.org'
+      case '012-cn.vuejs.org':
+      case 'v1-cn.vuejs.org':
+        return 'cn.vuejs.org'
+      case '012-jp.vuejs.org':
+      case 'v1-jp.vuejs.org':
+        return 'jp.vuejs.org'
+    }
+  }
+
+  location.host = toLatestHost(location.host)
+})()
