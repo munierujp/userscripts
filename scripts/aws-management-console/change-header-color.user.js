2e066ab9e8f10cef1ff2e936747951038bc8830f
Munieru <20086673+munierujp@users.noreply.github.com>
2020-10-14 15:10:30 +0900

    Update change-header-color.user.js

--- scripts/aws-management-console/change-header-color.user.js
+++ scripts/aws-management-console/change-header-color.user.js
@@ -1,7 +1,9 @@
+// @ts-check
+
 // ==UserScript==
 // @name         Change header color
 // @namespace    https://github.com/munierujp/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Change header color on AWS Management Console
 // @author       https://github.com/munierujp/
 // @homepageURL  https://github.com/munierujp/userscripts
@@ -14,6 +16,49 @@
 
 (function () {
   'use strict'
+
+  /** @typedef {'develop' | 'staging' | 'production'} Env */
+
+  /** @type {Record<string, Env>} */
+  const ACCOUNTS = {
+    '006841978030': 'production',
+    '264691882649': 'develop',
+    '273955500657': 'staging'
+  }
+
+  /** @type {Record<Env, string>} */
+  const COLORS = {
+    'develop': '#0D47A1',
+    'staging': '#1B5E20',
+    'production': '#B71C1C'
+  }
+
+  /**
+  * @param {string} env
+  * @returns {Env | undefined}
+  */
+  const normalizeEnv = (env) => {
+    switch (env) {
+      case 'develop':
+      case 'dev':
+        return 'develop'
+      case 'staging':
+      case 'stage':
+      case 'stg':
+        return 'staging'
+      case 'production':
+      case 'prod':
+      case 'prd':
+        return 'production'
+    }
+  }
+
+  const createStyleElement = (css) => {
+    const styleElement = document.createElement('style')
+    styleElement.type = 'text/css'
+    styleElement.textContent = css
+    return styleElement
+  }
 
   const createStyleElementByPatternA = () => {
     const accountNameElement = document.getElementById('awsc-login-display-name-account')
@@ -35,38 +80,21 @@
     }
 
     const suffix = matched[1]
-    let color
+    const env = normalizeEnv(suffix)
 
-    switch (suffix) {
-      case 'develop':
-      case 'dev':
-        color = '#0D47A1'
-        break
-      case 'staging':
-      case 'stage':
-      case 'stg':
-        color = '#1B5E20'
-        break
-      case 'production':
-      case 'prod':
-      case 'prd':
-        color = '#B71C1C'
-        break
-    }
-
-    if (!color) {
+    if (!env) {
       return
     }
 
-    const styleElement = document.createElement('style')
-    styleElement.type = 'text/css'
-    styleElement.textContent = `
+    const color = COLORS[env]
+
+    const styleElement = createStyleElement(`
 body #awsgnav #nav-menubar,
 body #awsgnav #nav-menubar .nav-menu,
 #nav-menu-right {
-background-color: ${color};
+  background-color: ${color};
 }
-`
+`)
     return styleElement
   }
 
@@ -83,31 +111,19 @@ background-color: ${color};
       return
     }
 
-    let color
+    const env = ACCOUNTS[accountId]
 
-    switch (accountId) {
-      case '264691882649':
-        color = '#0D47A1'
-        break
-      case '273955500657':
-        color = '#1B5E20'
-        break
-      case '006841978030':
-        color = '#B71C1C'
-        break
-    }
-
-    if (!color) {
+    if (!env) {
       return
     }
 
-    const styleElement = document.createElement('style')
-    styleElement.type = 'text/css'
-    styleElement.textContent = `
+    const color = COLORS[env]
+
+    const styleElement = createStyleElement(`
 [data-testid="awsc-nav-header-viewport-shelf-inner"] {
-background-color: ${color};
+  background-color: ${color};
 }
-`
+`)
     return styleElement
   }
 
