28ec37f4fecc363af7fb3df7cc526c0f8c0498e6
Munieru <20086673+munierujp@users.noreply.github.com>
2020-10-31 19:52:16 +0900

    add scripts/disk-station/video-station/play-video-with-vlc.user.js (#10)

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ My User Scripts
 - [change-header-color.user.js](https://munierujp.github.io/userscripts/scripts/aws-management-console/change-header-color.user.js)
 - [show-account-name-on-header.user.js](https://munierujp.github.io/userscripts/scripts/aws-management-console/show-account-name-on-header.user.js)
 
+### DiskStation
+
+#### Video Station
+
+- [play-video-with-vlc.user.js](https://munierujp.github.io/userscripts/scripts/disk-station/video-station/play-video-with-vlc.user.js)
+
 ### Vue.js
 
 - [redirect-to-latest-page.user.js](https://munierujp.github.io/userscripts/scripts/vuejs/redirect-to-latest-page.user.js)
---
+++ scripts/disk-station/video-station/play-video-with-vlc.user.js
@@ -0,0 +1,177 @@
+// @ts-check
+
+// ==UserScript==
+// @name         Play video with VLC
+// @namespace    https://github.com/munierujp/
+// @version      0.1.0
+// @description  Play video with VLC on Video Station of DiskStation
+// @author       https://github.com/munierujp/
+// @homepageURL  https://github.com/munierujp/userscripts
+// @updateURL    https://munierujp.github.io/userscripts/scripts/disk-station/video-station/play-video-with-vlc.user.js
+// @downloadURL  https://munierujp.github.io/userscripts/scripts/disk-station/video-station/play-video-with-vlc.user.js
+// @supportURL   https://github.com/munierujp/userscripts/issues
+// @match        https://diskstation.local:*/?launchApp=SYNO.SDS.VideoStation.AppInstance&SynoToken=*
+// @grant        none
+// ==/UserScript==
+
+// TODO: サムネイル上の小さい再生ボタンも書き換える
+
+(function () {
+  'use strict'
+
+  const URL_SCHEME = 'vlc'
+  const ROOT_DIR = '/Volumes'
+
+  /**
+   * @param {NodeList} nodeList
+   * @returns {Element[]}
+   */
+  const toElements = (nodeList) => {
+    /** @type {Element[]} */
+    // @ts-expect-error
+    const addedElements = Array.from(nodeList).filter(node => node instanceof Element)
+    return addedElements
+  }
+
+  /**
+   * @param {Element} element
+   * @returns {boolean}
+   */
+  const isPlayButtonElement = (element) => {
+    const { classList } = element
+    return classList.contains('x-btn') && classList.contains('play')
+  }
+
+  /**
+   * @param {Node} node
+   * @returns {Node}
+   */
+  const deepCloneNode = (node) => {
+    return node.cloneNode(true)
+  }
+
+  /**
+   * @param {Element} element
+   * @returns {boolean}
+   */
+  const isVideoInfoDialogElement = (element) => {
+    return element.classList.contains('video-info-dialog')
+  }
+
+  /**
+   * @param {Element} dialog
+   * @returns {string}
+   */
+  const findFilePath = (dialog) => {
+    return Array.from(dialog.querySelectorAll('tr'))
+      .map(row => Array.from(row.querySelectorAll('td')))
+      .filter(cells => cells.length >= 2)
+      .map(cells => cells.map(({ textContent }) => textContent))
+      .map(([label, value]) => ({
+        label,
+        value
+      }))
+      .filter(({ label }) => label === 'ファイルのパス')
+      .map(({ value }) => value)
+      .find(filePath => filePath)
+  }
+
+  /**
+   * @param {Element} dialog
+   */
+  const closeVideoInfoDialog = (dialog) => {
+    /** @type {HTMLButtonElement} */
+    const closeButton = dialog.querySelector('button[aria-label="閉じる"]')
+    closeButton.click()
+  }
+
+  // TODO: チラつきを防ぐために事前にmenuをCSSで非表示化してから実行し、実行後に非表示化を解除する
+  const openVideoInfoDialog = () => {
+    /** @type {HTMLButtonElement} */
+    const operationButton = document.querySelector('button[aria-label="操作"]')
+    operationButton.click()
+    /** @type {HTMLElement} */
+    const menu = document.querySelector('.syno-vs2-dropdown-menu')
+    /** @type {HTMLAnchorElement[]} */
+    const links = Array.from(menu.querySelectorAll('a.x-menu-list-item'))
+    const link = links.find(({ textContent }) => textContent === 'メディア情報を表示')
+    link.click()
+  }
+
+  // TODO: チラつきを防ぐために事前にdialogをCSSで非表示化してから実行し、実行後に非表示化を解除する
+  /**
+   * @returns {Promise<string>}
+   */
+  const fetchFilePath = () => {
+    return new Promise((resolve) => {
+      const observer = new MutationObserver((records, observer) => {
+        records
+          .filter(({ addedNodes }) => addedNodes)
+          .map(({ addedNodes }) => toElements(addedNodes))
+          .map(elements => elements.filter(isVideoInfoDialogElement))
+          .filter(dialogs => dialogs.length)
+          .forEach(dialogs => {
+            dialogs.forEach(dialog => {
+              const filePath = findFilePath(dialog)
+              resolve(filePath)
+              console.debug('close video info dialog')
+              closeVideoInfoDialog(dialog)
+            })
+            observer.disconnect()
+          })
+      })
+      observer.observe(document.getElementById('sds-desktop'), {
+        childList: true
+      })
+      console.debug('open video info dialog')
+      openVideoInfoDialog()
+    })
+  }
+
+  /**
+   * @param {Element} playButton
+   */
+  const replacePlayButton = (playButton) => {
+    const playWithVlcButton = deepCloneNode(playButton)
+    playWithVlcButton.addEventListener('click', () => {
+      console.debug('fetch file path')
+      fetchFilePath()
+        .then(filePath => {
+          console.debug(`filePath=${filePath}`)
+          const url = `${URL_SCHEME}://${ROOT_DIR}${filePath}`
+          console.debug(`url=${url}`)
+          return url
+        })
+        .then(url => {
+          console.debug(`open ${url}`)
+          window.open(url)
+        })
+    })
+    playButton.parentElement.appendChild(playWithVlcButton)
+    playButton.parentElement.removeChild(playButton)
+  }
+
+  const main = () => {
+    console.debug('start')
+    const observer = new MutationObserver((records, observer) => {
+      records
+        .filter(({ addedNodes }) => addedNodes)
+        .map(({ addedNodes }) => toElements(addedNodes))
+        .map(elements => elements.filter(isPlayButtonElement))
+        .filter(playButtons => playButtons.length)
+        .forEach(playButtons => {
+          playButtons.forEach(playButton => {
+            console.debug('replace play button')
+            replacePlayButton(playButton)
+          })
+          observer.disconnect()
+        })
+    })
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    })
+  }
+
+  main()
+})()
