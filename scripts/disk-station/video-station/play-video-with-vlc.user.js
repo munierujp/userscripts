5a3da54694243d84f3cd4f4230b32d5149ea0eed
Munieru <20086673+munierujp@users.noreply.github.com>
2020-11-01 22:44:17 +0900

    patch

--- scripts/disk-station/video-station/play-video-with-vlc.user.js
+++ scripts/disk-station/video-station/play-video-with-vlc.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         Play video with VLC
 // @namespace    https://github.com/munierujp/
-// @version      0.1.6
+// @version      0.1.7
 // @description  Play video with VLC on Video Station of DiskStation
 // @author       https://github.com/munierujp/
 // @homepageURL  https://github.com/munierujp/userscripts
