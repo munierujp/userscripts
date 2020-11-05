53c128ab49a72874cc23ce53669aae6d8a56e5d8
Munieru <20086673+munierujp@users.noreply.github.com>
2020-11-05 23:26:35 +0900

    update add-filter-button-on-bookmark-page.user.js to v1.0.0

--- scripts/dmm/add-filter-button-on-bookmark-page.user.js
+++ scripts/dmm/add-filter-button-on-bookmark-page.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name        Add filter button on bookmark page
 // @namespace    https://github.com/munierujp/
-// @version      0.1.1
+// @version      1.0.0
 // @description   Add filter button on bookmark page on DMM Books
 // @author       https://github.com/munierujp/
 // @homepageURL  https://github.com/munierujp/userscripts
