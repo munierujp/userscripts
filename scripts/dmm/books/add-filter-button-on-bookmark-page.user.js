42a29e7ece0d17c66fb5937a4fc06643e32fcca7
Munieru <20086673+munierujp@users.noreply.github.com>
2020-11-08 01:34:32 +0900

    Update add-filter-button-on-bookmark-page.user.js to v1.1.0 (#24)
    * rename
    
    * sort
    
    * update showDiscountedItems
    
    * add STYLE_DISPLAY_ITEM_SHOW
    
    * add STYLE_DISPLAY_ITEM_HIDDEN
    
    * beautify
    
    * add getMenuElement
    
    * add getViewStyle(
    
    * update main
    
    * add comments
    
    * rename
    
    * rename
    
    * rename isCurrentButton to isCurrentElement
    
    * refactor getViewStyle
    
    * update comment
    
    * add getMainElement
    
    * support list view
    
    * refactor
    
    * remove unused values
    
    * refactor
    
    * refactor
    
    * rename
    
    * rename
    
    * add createFilterMenuElementForListView
    
    * add createFilterMenuElementForThumbnailView
    
    * add comment
    
    * update getViewStyle
    
    * remove unused values
    
    * update ViewStyle
    
    * remove comment
    
    * refactor
    
    * sort
    
    * rename
    
    * rename ViewStyle to ViewType
    
    * add ShowItemsFunction
    
    * update getViewType
    
    * refactor
    
    * add appendFilterMenu
    
    * minor
    
    * add createButtonLabelElement
    
    * add createMenuLabelElement
    
    * refactor
    
    * add createFilterMenuElement
    
    * remove appendFilterMenu

--- scripts/dmm/books/add-filter-button-on-bookmark-page.user.js
+++ scripts/dmm/books/add-filter-button-on-bookmark-page.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name        Add filter button on bookmark page
 // @namespace    https://github.com/munierujp/
-// @version      1.0.7
+// @version      1.1.0
 // @description   Add filter button on bookmark page on DMM Books
 // @author       https://github.com/munierujp/
 // @homepageURL  https://github.com/munierujp/userscripts
@@ -15,14 +15,59 @@
 // @grant        none
 // ==/UserScript==
 
-// TODO: リスト表示に対応
-
 (function () {
   'use strict'
 
-  const CLASS_BUTTON_INACTIVE = 'current'
-  const CURSOR_BUTTON_ACTIVE = 'pointer'
-  const CURSOR_BUTTON_INACTIVE = 'auto'
+  /** @typedef {'table' | 'list'} ViewType */
+  /** @typedef {() => void} ShowItemsFunction */
+
+  const CLASS_CURRENT = 'current'
+  const CURSOR_BUTTON_CURRENT = 'auto'
+  const CURSOR_BUTTON_NOT_CURRENT = 'pointer'
+
+  /**
+   * @returns {ViewType}
+   */
+  const getViewType = () => {
+    const params = new URLSearchParams(location.search)
+    const view = params.get('view')
+
+    switch (view) {
+      case 'table':
+        return 'table'
+      case 'list':
+        return 'list'
+      default:
+        throw new Error('Failed to get view type.')
+    }
+  }
+
+  /**
+   * @returns {HTMLElement}
+   */
+  const getMainElement = () => {
+    return document.getElementById('main-bmk')
+  }
+
+  /**
+   * @param {HTMLElement} main
+   * @returns {HTMLElement}
+   */
+  const findMenuElement = (main) => {
+    return main.querySelector('.d-rcol.selector')
+  }
+
+  /**
+   * @param {string} text
+   * @returns {HTMLSpanElement}
+   */
+  const createButtonLabelElement = (text) => {
+    const label = document.createElement('span')
+    label.style['padding-left'] = '8px'
+    label.style['padding-right'] = '8px'
+    label.textContent = text
+    return label
+  }
 
   /**
    * @param {string} text
@@ -31,10 +76,7 @@
   const createButtonElement = (text) => {
     const button = document.createElement('li')
     button.style.width = 'auto'
-    const label = document.createElement('span')
-    label.style['padding-left'] = '8px'
-    label.style['padding-right'] = '8px'
-    label.textContent = text
+    const label = createButtonLabelElement(text)
     button.appendChild(label)
     return button
   }
@@ -43,10 +85,10 @@
    * @param {string} text
    * @returns {HTMLLIElement}
    */
-  const createInactiveButtonElement = (text) => {
+  const createCurrentButtonElement = (text) => {
     const button = createButtonElement(text)
-    button.classList.add(CLASS_BUTTON_INACTIVE)
-    button.style.cursor = CURSOR_BUTTON_INACTIVE
+    button.classList.add(CLASS_CURRENT)
+    button.style.cursor = CURSOR_BUTTON_CURRENT
     return button
   }
 
@@ -54,73 +96,54 @@
    * @param {string} text
    * @returns {HTMLLIElement}
    */
-  const createActiveButtonElement = (text) => {
+  const createNotCurrentButtonElement = (text) => {
     const button = createButtonElement(text)
-    button.style.cursor = CURSOR_BUTTON_ACTIVE
+    button.style.cursor = CURSOR_BUTTON_NOT_CURRENT
     return button
   }
 
   /**
-   * @param {HTMLElement} button
+   * @param {HTMLElement} element
+   * @returns {boolean}
    */
-  const isInactiveButton = (button) => {
-    return button.classList.contains(CLASS_BUTTON_INACTIVE)
-  }
-
-  /**
-   * @param {HTMLElement} button
-   */
-  const activateButton = (button) => {
-    button.classList.remove(CLASS_BUTTON_INACTIVE)
-    button.style.cursor = CURSOR_BUTTON_ACTIVE
+  const isCurrentElement = (element) => {
+    return element.classList.contains(CLASS_CURRENT)
   }
 
   /**
    * @param {HTMLElement} button
    */
   const deactivateButton = (button) => {
-    button.classList.add(CLASS_BUTTON_INACTIVE)
-    button.style.cursor = CURSOR_BUTTON_INACTIVE
+    button.classList.add(CLASS_CURRENT)
+    button.style.cursor = CURSOR_BUTTON_CURRENT
   }
 
   /**
-   * @returns {HTMLLIElement[]}
+   * @param {HTMLElement} button
    */
-  const getItemElements = () => {
-    const list = document.getElementById('list')
-    const items = Array.from(list.querySelectorAll('li'))
-    return items
-  }
-
-  const showAllItems = () => {
-    const items = getItemElements()
-    items.forEach(item => {
-      item.style.display = 'list-item'
-    })
+  const activateButton = (button) => {
+    button.classList.remove(CLASS_CURRENT)
+    button.style.cursor = CURSOR_BUTTON_NOT_CURRENT
   }
 
   /**
-   * @param {number} [rate]
-   */
-  const showDiscountedItems = (rate) => {
-    const items = getItemElements()
-    items.forEach(item => {
-      const discount = item.querySelector('.txtoff')
-      const show = rate ? (discount && discount.textContent === `${rate}%OFF`) : discount
-      const display = show ? 'list-item' : 'none'
-      item.style.display = display
-    })
-  }
-
-  /**
+   * @param {Object} params
+   * @param {ShowItemsFunction} params.showAllItems
+   * @param {ShowItemsFunction} params.showDiscountedItems
    * @returns {HTMLUListElement}
    */
-  const createButtonListElement = () => {
-    const showAllButton = createInactiveButtonElement('すべて')
-    const showDiscountedButton = createActiveButtonElement('セール中')
+  const createButtonListElement = ({
+    showAllItems,
+    showDiscountedItems
+  }) => {
+    const buttonList = document.createElement('ul')
+    const showAllButton = createCurrentButtonElement('すべて')
+    buttonList.appendChild(showAllButton)
+    const showDiscountedButton = createNotCurrentButtonElement('セール中')
+    buttonList.appendChild(showDiscountedButton)
 
     showAllButton.addEventListener('click', () => {
-      if (isInactiveButton(showAllButton)) {
+      if (isCurrentElement(showAllButton)) {
         return
       }
 
@@ -130,7 +153,7 @@
     })
 
     showDiscountedButton.addEventListener('click', () => {
-      if (isInactiveButton(showDiscountedButton)) {
+      if (isCurrentElement(showDiscountedButton)) {
         return
       }
 
@@ -139,30 +162,109 @@
       showDiscountedItems()
     })
 
-    const buttonList = document.createElement('ul')
-    buttonList.appendChild(showAllButton)
-    buttonList.appendChild(showDiscountedButton)
     return buttonList
   }
 
   /**
-   * @returns {HTMLDivElement}
+   * @param {string} text
+   * @returns {HTMLSpanElement}
    */
-  const createFilterMenuElement = () => {
+  const createMenuLabelElement = (text) => {
     const label = document.createElement('span')
-    label.textContent = '絞り込み'
-    const buttonList = createButtonListElement()
+    label.textContent = text
+    return label
+  }
+
+  /**
+   * @param {Object} params
+   * @param {ShowItemsFunction} params.showAllItems
+   * @param {ShowItemsFunction} params.showDiscountedItems
+   */
+  const createFilterMenuElement = ({
+    showAllItems,
+    showDiscountedItems
+  }) => {
     const filterMenu = document.createElement('div')
+    const label = createMenuLabelElement('絞り込み')
     filterMenu.appendChild(label)
+    const buttonList = createButtonListElement({
+      showAllItems,
+      showDiscountedItems
+    })
     filterMenu.appendChild(buttonList)
     return filterMenu
   }
 
+  const appendFilterMenuOnTableView = () => {
+    const main = getMainElement()
+    const list = main.querySelector('#list')
+    const items = Array.from(list.querySelectorAll('li'))
+    const showAllItems = () => {
+      items.forEach(item => {
+        item.style.display = 'list-item'
+      })
+    }
+    const showDiscountedItems = () => {
+      items.forEach(item => {
+        const discount = item.querySelector('.txtoff')
+        const show = discount !== null
+        const display = show ? 'list-item' : 'none'
+        item.style.display = display
+      })
+    }
+    const filterMenu = createFilterMenuElement({
+      showAllItems,
+      showDiscountedItems
+    })
+    const menu = findMenuElement(main)
+    menu.appendChild(filterMenu)
+  }
+
+  const appendFilterMenuOnListView = () => {
+    const main = getMainElement()
+    const table = main.querySelector('table')
+    const rows = Array.from(table.querySelectorAll('tr'))
+      .filter(row => row.querySelector('td'))
+    const showAllItems = () => {
+      rows.forEach(row => {
+        row.style.display = 'table-row'
+      })
+    }
+    const showDiscountedItems = () => {
+      rows.forEach(row => {
+        const price = row.querySelector('.price')
+        const discount = price.querySelector('.tx-sp')
+        const show = discount !== null
+        const display = show ? 'table-row' : 'none'
+        row.style.display = display
+      })
+    }
+    const filterMenu = createFilterMenuElement({
+      showAllItems,
+      showDiscountedItems
+    })
+    const menu = findMenuElement(main)
+    menu.appendChild(filterMenu)
+  }
+
+  /**
+   * @param {ViewType} viewType
+   */
+  const getAppendFilterMenuFunction = (viewType) => {
+    switch (viewType) {
+      case 'table':
+        return appendFilterMenuOnTableView
+      case 'list':
+        return appendFilterMenuOnListView
+    }
+  }
+
   const main = () => {
     console.debug('start')
-    const menu = document.querySelector('.d-rcol.selector')
-    const filterMenu = createFilterMenuElement()
-    menu.appendChild(filterMenu)
+    const viewType = getViewType()
+    console.debug(`viewType=${viewType}`)
+    const appendFilterMenu = getAppendFilterMenuFunction(viewType)
+    appendFilterMenu()
   }
 
   main()
