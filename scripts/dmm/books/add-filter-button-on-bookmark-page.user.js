4abbbda62d2f50ab20e2874486a4055600ba62e0
Munieru <20086673+munierujp@users.noreply.github.com>
2020-11-12 21:42:31 +0900

    remove createButtonLabelElement

--- scripts/dmm/books/add-filter-button-on-bookmark-page.user.js
+++ scripts/dmm/books/add-filter-button-on-bookmark-page.user.js
@@ -76,24 +76,15 @@
 
   /**
    * @param {string} text
-   * @returns {HTMLSpanElement}
-   */
-  const createButtonLabelElement = (text) => {
-    const label = document.createElement('span')
-    label.style['padding-left'] = '8px'
-    label.style['padding-right'] = '8px'
-    label.textContent = text
-    return label
-  }
-
-  /**
-   * @param {string} text
    * @returns {HTMLLIElement}
    */
   const createButtonElement = (text) => {
     const button = document.createElement('li')
     button.style.width = 'auto'
-    const label = createButtonLabelElement(text)
+    const label = document.createElement('span')
+    label.style['padding-left'] = '8px'
+    label.style['padding-right'] = '8px'
+    label.textContent = text
     button.appendChild(label)
     return button
   }
