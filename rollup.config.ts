30c8456023e4814a7dc15bbfec44abe02a8ea746
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-12 01:18:29 +0900

    Add meta.txt files

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,50 +1,22 @@
+import { readFile } from 'fs/promises'
 import typescript from '@rollup/plugin-typescript'
 import type { RollupOptions } from 'rollup'
 
-const config: RollupOptions[] = [
-  {
-    input: 'src/amazon/add-bought-kindle-book-to-booklog/main.ts',
+const createOptions = (id: string): RollupOptions => {
+  return {
+    input: `src/${id}/main.ts`,
     output: {
-      file: 'dist/amazon/add-bought-kindle-book-to-booklog.user.js',
+      file: `dist/${id}.user.js`,
       format: 'iife',
-      banner: `// ==UserScript==
-// @name        Kindle本の購入完了時にブクログに登録
-// @namespace    https://github.com/munierujp/
-// @version      0.2.0
-// @description   AmazonでKindle本の購入完了時にブクログに読書状況を積読として登録します。
-// @author       https://github.com/munierujp/
-// @homepageURL  https://github.com/munierujp/userscripts
-// @updateURL    https://github.com/munierujp/userscripts/raw/master/dist/amazon/add-bought-kindle-book-to-booklog.user.js
-// @downloadURL  https://github.com/munierujp/userscripts/raw/master/dist/amazon/add-bought-kindle-book-to-booklog.user.js
-// @supportURL   https://github.com/munierujp/userscripts/issues
-// @match        https://www.amazon.co.jp/kindle-dbs/thankYouPage?*
-// @match        https://booklog.jp/item/1/*
-// @grant        none
-// ==/UserScript==`
-    },
-    plugins: [typescript()]
-  },
-  {
-    input: 'src/amazon/open-internal-links-in-current-tab/main.ts',
-    output: {
-      file: 'dist/amazon/open-internal-links-in-current-tab.user.js',
-      format: 'iife',
-      banner: `// ==UserScript==
-// @name        サイト内リンクを現在のタブで開く
-// @namespace    https://github.com/munierujp/
-// @version      0.2.0
-// @description   Amazonでサイト内リンクを現在のタブで開きます。
-// @author       https://github.com/munierujp/
-// @homepageURL  https://github.com/munierujp/userscripts
-// @updateURL    https://github.com/munierujp/userscripts/raw/master/dist/amazon/open-internal-links-in-current-tab.user.js
-// @downloadURL  https://github.com/munierujp/userscripts/raw/master/dist/amazon/open-internal-links-in-current-tab.user.js
-// @supportURL   https://github.com/munierujp/userscripts/issues
-// @match        https://www.amazon.co.jp/*
-// @grant        none
-// ==/UserScript==`
+      banner: async () => await readFile(`src/${id}/meta.txt`, 'utf8')
     },
     plugins: [typescript()]
   }
+}
+
+const config: RollupOptions[] = [
+  createOptions('amazon/add-bought-kindle-book-to-booklog'),
+  createOptions('amazon/open-internal-links-in-current-tab')
 ]
 
 export default config
