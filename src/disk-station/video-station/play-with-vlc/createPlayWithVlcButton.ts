dfda5b757a58b1f69a0a4c40a85975b4693627cd
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-12 23:48:24 +0900

    Refactoring createPlayWithVlcButton

--- src/disk-station/video-station/play-with-vlc/createPlayWithVlcButton.ts
+++ src/disk-station/video-station/play-with-vlc/createPlayWithVlcButton.ts
@@ -2,7 +2,11 @@ import { createUrl } from './createUrl'
 import { fetchFilePath } from './fetchFilePath'
 
 export const createPlayWithVlcButton = (playButton: HTMLElement): HTMLElement => {
-  const playWithVlcButton = playButton.cloneNode(true) as HTMLElement
+  const playWithVlcButton = playButton.cloneNode(true)
+
+  if (!(playWithVlcButton instanceof HTMLElement)) {
+    throw new TypeError('Failed to clone node.')
+  }
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   playWithVlcButton.addEventListener('click', async () => {
     const filePath = await fetchFilePath()
