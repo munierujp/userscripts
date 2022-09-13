43318ee54ac0f6c4b8c2d63a3650fa4c4bace7fb
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-13 22:57:31 +0900

    Update extractAsin.test.ts

--- src/amazon/normalize-internal-links/extractAsin.test.ts
+++ src/amazon/normalize-internal-links/extractAsin.test.ts
@@ -1,10 +1,11 @@
 import { extractAsin } from './extractAsin'
 
 describe('extractAsin', () => {
-  it('returns ASIN if it exists', () => {
-    const expected = 'B0064CZ1XE'
-    const path = `/dp/${expected}`
-
+  it.each([
+    ['/dp/B0064CZ1XE', 'B0064CZ1XE'],
+    ['/foo/dp/B0064CZ1XE', 'B0064CZ1XE'],
+    ['/gp/product/B0064CZ1XE', 'B0064CZ1XE']
+  ])('returns ASIN if it exists', (path, expected) => {
     const actual = extractAsin(path)
 
     expect(actual).toBe(expected)
