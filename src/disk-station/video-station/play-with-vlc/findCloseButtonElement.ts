d70912ebd7a16f8865f99b7bfce07a9e7c4c8027
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-15 03:22:56 +0900

    Update findCloseButtonElement

--- src/disk-station/video-station/play-with-vlc/findCloseButtonElement.ts
+++ src/disk-station/video-station/play-with-vlc/findCloseButtonElement.ts
@@ -1,4 +1,4 @@
 // TODO: リファクタリング
-export const findCloseButtonElement = (dialog: HTMLElement): HTMLButtonElement | undefined => {
+export const findCloseButtonElement = (dialog: Element): HTMLButtonElement | undefined => {
   return dialog.querySelector<HTMLButtonElement>('button[aria-label="閉じる"]') ?? undefined
 }
