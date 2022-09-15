0f81aed4ef0834a2ebc24f1e438a82e899cbcf98
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-15 23:34:25 +0900

    Update cloneNode.test.ts

--- src/lib/cloneNode.test.ts
+++ src/lib/cloneNode.test.ts
@@ -2,12 +2,31 @@ import { cloneNode } from './cloneNode'
 
 describe('cloneNode', () => {
   it('returns cloned node', () => {
-    const element = document.createElement('a')
-    element.setAttribute('id', 'test')
+    const parentNode = document.createElement('div')
+    parentNode.setAttribute('id', 'parent')
+    parentNode.classList.add('before')
 
-    const actual = cloneNode(element)
+    const childNode = document.createElement('span')
+    childNode.setAttribute('id', 'child')
+    childNode.classList.add('before')
+    parentNode.append(childNode)
 
-    expect(actual).toBeInstanceOf(HTMLAnchorElement)
-    expect(actual.getAttribute('id')).toBe('test')
+    const clonedParentNode = cloneNode(parentNode)
+    parentNode.classList.remove('before')
+    parentNode.classList.add('after')
+    childNode.classList.remove('before')
+    childNode.classList.add('after')
+
+    expect(clonedParentNode).toBeInstanceOf(HTMLDivElement)
+    expect(clonedParentNode.getAttribute('id')).toEqual('parent')
+    expect(clonedParentNode.classList.contains('before')).toBe(true)
+    expect(clonedParentNode.classList.contains('after')).toBe(false)
+    expect(clonedParentNode.children).toHaveLength(1)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const clonedChildNode = clonedParentNode.children[0]!
+    expect(clonedChildNode).toBeInstanceOf(HTMLSpanElement)
+    expect(clonedChildNode.getAttribute('id')).toEqual('child')
+    expect(clonedChildNode.classList.contains('before')).toBe(true)
+    expect(clonedChildNode.classList.contains('after')).toBe(false)
   })
 })
