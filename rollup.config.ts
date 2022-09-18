7eefcfc9ba54bf92fd520e4b4a4699c1be694155
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-18 21:56:14 +0900

    Update rollup config (#153)
    * Add glob
    
    * Update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,28 +1,19 @@
-import { readFile } from 'node:fs/promises'
+import { readFileSync } from 'node:fs'
 import typescript from '@rollup/plugin-typescript'
+import { sync as globSync } from 'glob'
 import type { RollupOptions } from 'rollup'
 
-const createOptions = (id: string): RollupOptions => {
-  return {
-    input: `src/${id}/main.ts`,
-    output: {
-      file: `dist/${id}.user.js`,
-      format: 'iife',
-      banner: async () => await readFile(`src/${id}/meta.txt`, 'utf8')
-    },
-    plugins: [
-      typescript()
-    ]
-  }
-}
-
-const config: RollupOptions[] = [
-  createOptions('amazon/add-bought-kindle-book-to-booklog'),
-  createOptions('amazon/normalize-internal-links'),
-  createOptions('amazon/open-internal-links-in-current-tab'),
-  createOptions('amazon/redirect-to-normalized-url'),
-  createOptions('dmm/books/add-filter-button-on-bookmark-page'),
-  createOptions('disk-station/video-station/play-with-vlc')
-]
+const files = globSync('src/**/main.ts')
+const config: RollupOptions[] = files.map(file => ({
+  input: file,
+  output: {
+    file: file.replace(/^src\//, 'dist/').replace(/\/main\.ts$/, '.user.js'),
+    format: 'iife',
+    banner: readFileSync(file.replace(/\/main\.ts$/, '/meta.txt'), 'utf8')
+  },
+  plugins: [
+    typescript()
+  ]
+}))
 
 export default config
