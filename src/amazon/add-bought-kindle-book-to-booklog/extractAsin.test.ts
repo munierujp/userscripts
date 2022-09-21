69555929ff0680b9f4fafbdee032f77db8d38b88
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-22 01:47:08 +0900

    Use @jest/globals

--- src/amazon/add-bought-kindle-book-to-booklog/extractAsin.test.ts
+++ src/amazon/add-bought-kindle-book-to-booklog/extractAsin.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import { extractAsin } from './extractAsin'
 
 describe('extractAsin', () => {
--- src/disk-station/video-station/play-with-vlc/isPlayButton.test.ts
+++ src/disk-station/video-station/play-with-vlc/isPlayButton.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import { isPlayButton } from './isPlayButton'
 
 describe('isPlayButton', () => {
