4b3e460437ae31a0bf462f7b4e30241e4398535a
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-22 01:48:14 +0900

    Remove @types/jest (#162)
    * Use @jest/globals
    
    * Remove @types/jest

--- src/amazon/redirect-to-normalized-url/createUrl.test.ts
+++ src/amazon/redirect-to-normalized-url/createUrl.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import { createUrl } from './createUrl'
 
 describe('createUrl', () => {
--- src/disk-station/lib/isVideoStationPage.test.ts
+++ src/disk-station/lib/isVideoStationPage.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import { isVideoStationPage } from './isVideoStationPage'
 
 describe('isVideoStationPage', () => {
