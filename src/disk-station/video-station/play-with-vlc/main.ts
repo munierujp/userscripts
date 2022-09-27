6c99b3730f5779f027f0ace2d1085ab451a5d458
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-28 00:48:25 +0900

    Create index.ts

--- src/disk-station/video-station/play-with-vlc/main.ts
+++ src/disk-station/video-station/play-with-vlc/main.ts
@@ -1,4 +1,4 @@
-import { isVideoStationPage } from '../../lib/isVideoStationPage'
+import { isVideoStationPage } from '../../lib'
 import { handleVideoStation } from './handleVideoStation'
 
 if (isVideoStationPage(new URL(location.href))) {
