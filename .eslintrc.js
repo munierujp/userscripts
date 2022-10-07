e7fae0b26f0146c4d3f291b3c0ef8450e100370c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-07 20:13:54 +0900

    Use @munierujp/eslint-config-typescript (#186)
    * Update eslint-plugin-unicorn to v44.0.0
    
    * Add @eslint-recommended/eslint-config-typescript
    
    * Update ESLint config
    
    * Remove eslint-config-standard-with-typescript
    
    * Remove eslint-plugin-eslint-comments
    
    * Remove eslint-plugin-unicorn
    
    * Pin @eslint-recommended/eslint-config-typescript version
    
    * Add @types/eslint
    
    * Downgrade eslint to v8.4.1
    
    * Update ESLint config
    
    * Use @munierujp/eslint-config-typescript
    
    * Update ESLint config
    
    * Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,15 @@
-const unicornRules = {
-  // ファイル名のケースはものによって異なる（関数はローワーキャメルケース、クラスはアッパーキャメルケースなど）ので無効化
-  'unicorn/filename-case': 'off',
-  // Array.prototype.forEach() を使うのは理にかなっているので無効化
-  'unicorn/no-array-for-each': 'off',
-  // undefinedを明示的に書くのは理にかなっているので無効化
-  'unicorn/no-useless-undefined': 'off',
-  // 数値を 1_000 のように書くと読みやすいケースもあるもののそうではないケースもあるので無効化
-  'unicorn/numeric-separators-style': 'off',
-  // 完全にESMにはできないので無効化
-  'unicorn/prefer-module': 'off',
-  // セレクタによって要素の取得メソッドを使いわけるのは理にかなっているので無効化
-  'unicorn/prefer-query-selector': 'off',
-  // Array.from() を使うのは理にかなっているので無効化
-  'unicorn/prefer-spread': 'off',
-  // TODO [engine:node@>=18]: Node.js v18以上になったら有効化
-  'unicorn/prefer-top-level-await': 'off',
-  // 一般的に定着しているような略語を省略せずに書くのは冗長なので無効化
-  'unicorn/prevent-abbreviations': 'off'
-}
-
-const importRules = {
-  'sort-imports': 'off',
-  'import/order': ['error', {
-    alphabetize: {
-      order: 'asc',
-      caseInsensitive: true
-    }
-  }],
-  'import-newlines/enforce': ['error', {
-    items: 1,
-    semi: false
-  }]
-}
-
 module.exports = {
   overrides: [
     {
       files: [
-        '**/*.js',
-        '**/*.ts'
+        '*.js',
+        '*.ts'
       ],
       excludedFiles: [
         'dist/**/*.js'
       ],
       extends: [
-        'standard-with-typescript',
-        'plugin:unicorn/recommended',
-        'plugin:eslint-comments/recommended',
+        '@munierujp/eslint-config-typescript',
         'plugin:jest/recommended'
       ],
       plugins: [
@@ -60,8 +23,6 @@ module.exports = {
         browser: true
       },
       rules: {
-        ...unicornRules,
-        ...importRules,
         'tsdoc/syntax': 'warn'
       }
     },
