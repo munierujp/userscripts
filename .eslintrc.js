5d9c1fd837cde423ba38dc40b2142f93c592677b
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-07 20:02:07 +0900

    Use @munierujp/eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,4 @@
-// @ts-check
-
-/** @typedef {import("eslint").Linter.BaseConfig } BaseConfig */
-/** @typedef {import("eslint").Linter.RulesRecord } RulesRecord */
-
-/** @type {RulesRecord} */
-const unicornRules = {
-  // ファイル名のケースはものによって異なる（関数はローワーキャメルケース、クラスはアッパーキャメルケースなど）ので無効化
-  'unicorn/filename-case': 'off',
-  // Array.prototype.forEach() を使うのは理にかなっているので無効化
-  'unicorn/no-array-for-each': 'off',
-  // undefinedを明示的に書くのは理にかなっているので無効化
-  'unicorn/no-useless-undefined': 'off',
-  // 数値を 1_000 のように書くと読みやすいケースもあるもののそうではないケースもあるので無効化
-  'unicorn/numeric-separators-style': 'off',
-  // 完全にESMにはできないので無効化
-  'unicorn/prefer-module': 'off',
-  // セレクタによって要素の取得メソッドを使いわけるのは理にかなっているので無効化
-  'unicorn/prefer-query-selector': 'off',
-  // Array.from() を使うのは理にかなっているので無効化
-  'unicorn/prefer-spread': 'off',
-  // TODO [engine:node@>=18]: Node.js v18以上になったら有効化
-  'unicorn/prefer-top-level-await': 'off',
-  // 一般的に定着しているような略語を省略せずに書くのは冗長なので無効化
-  'unicorn/prevent-abbreviations': 'off',
-  // switch文のcase節を常にブロックにするのは冗長なので必要なときのみブロックにする
-  'unicorn/switch-case-braces': ['error', 'avoid']
-}
-
-/** @type {RulesRecord} */
-const importRules = {
-  'sort-imports': 'off',
-  'import/order': ['error', {
-    alphabetize: {
-      order: 'asc',
-      caseInsensitive: true
-    }
-  }],
-  'import-newlines/enforce': ['error', {
-    items: 1,
-    semi: false
-  }]
-}
-
-/** @type {BaseConfig} */
-const config = {
+module.exports = {
   overrides: [
     {
       files: [
@@ -54,7 +9,7 @@ const config = {
         'dist/**/*.js'
       ],
       extends: [
-        '@eslint-recommended/eslint-config-typescript',
+        '@munierujp/eslint-config-typescript',
         'plugin:jest/recommended'
       ],
       plugins: [
@@ -65,10 +20,6 @@ const config = {
       },
       env: {
         browser: true
-      },
-      rules: {
-        ...unicornRules,
-        ...importRules
       }
     },
     {
@@ -98,5 +49,3 @@ const config = {
     }
   ]
 }
-
-module.exports = config
