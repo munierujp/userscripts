a61f9e7b124cc0866788a8816aad309e0dd3513c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-12 23:57:32 +0900

    Add comments

--- src/amazon/normalize-internal-links/extractAsin.ts
+++ src/amazon/normalize-internal-links/extractAsin.ts
@@ -1,3 +1,8 @@
+/**
+ * パスからASINを抽出します。
+ * @param path - パス
+ * @returns ASIN
+ */
 export const extractAsin = (path: string): string | undefined => {
   return path.match(/^\/(gp\/product|(([^/]+)\/)?dp)\/([^/?]+)([/?].+)?/)?.[4]
 }
