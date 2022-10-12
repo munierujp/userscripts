edd9bac090fedbd7aff4c7e6dd738a36ab2f611c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-13 00:08:35 +0900

    Add comments

--- src/amazon/open-internal-links-in-current-tab/findInternalLinkElements.ts
+++ src/amazon/open-internal-links-in-current-tab/findInternalLinkElements.ts
@@ -1,3 +1,7 @@
+/**
+ * 内部リンク要素を取得します。
+ * @returns 内部リンク要素
+ */
 export const findInternalLinkElements = (): HTMLAnchorElement[] => {
   return Array.from(document.querySelectorAll('a[href^="/"][target="_blank"]'))
 }
