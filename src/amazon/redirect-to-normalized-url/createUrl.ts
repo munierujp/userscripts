ab61b4c720b06b786afbacab19fea10783cc2e31
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-13 00:10:01 +0900

    Add comments

--- src/amazon/redirect-to-normalized-url/createUrl.ts
+++ src/amazon/redirect-to-normalized-url/createUrl.ts
@@ -1,3 +1,8 @@
+/**
+ * ASINをもとに商品ページのURLを作成します。
+ * @param asin - ASIN
+ * @returns URL
+ */
 export const createUrl = (asin: string): string => {
   return `https://www.amazon.co.jp/dp/${asin}`
 }
--- src/amazon/redirect-to-normalized-url/extractAsin.ts
+++ src/amazon/redirect-to-normalized-url/extractAsin.ts
@@ -1,3 +1,8 @@
+/**
+ * URLからASINを抽出します。
+ * @param url - URL
+ * @returns ASIN
+ */
 export const extractAsin = (url: string): string | undefined => {
   return url.match(/^https?:\/\/www\.amazon\.co\.jp\/(gp\/product|(([^/]+)\/)?dp)\/([^/?]+)([/?].+)?/)?.[4]
 }
