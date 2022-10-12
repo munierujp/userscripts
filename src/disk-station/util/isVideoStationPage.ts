8bd7bb46a34c37e120415278d4f800799600330f
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-13 00:14:17 +0900

    Add comments

--- src/disk-station/util/isVideoStationPage.ts
+++ src/disk-station/util/isVideoStationPage.ts
@@ -1,3 +1,8 @@
+/**
+ * URLがVideo Stationかどうかを返します。
+ * @param url - URL
+ * @returns URLがVideo Stationかどうか
+ */
 export const isVideoStationPage = (url: URL): boolean => {
   return url.searchParams.get('launchApp') === 'SYNO.SDS.VideoStation.AppInstance'
 }
