f69a0d912666d0a918f24f4a41e8cddcff7b0cd3
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-13 00:35:14 +0900

    Bump up version

--- dist/disk-station/video-station/play-with-vlc.user.js
+++ dist/disk-station/video-station/play-with-vlc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VLCで再生
 // @namespace    https://github.com/munierujp/
-// @version      1.1.3
+// @version      1.1.4
 // @description  DiskStationのVideo Stationで動画をVLCで再生します。
 // @author       https://github.com/munierujp/
 // @homepage     https://github.com/munierujp/userscripts
