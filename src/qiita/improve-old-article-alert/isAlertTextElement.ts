0115216cbddb23e4d78b08edf0b659709f496827
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-16 12:20:00 +0900

    Update isAlertTextElement

--- src/qiita/improve-old-article-alert/isAlertTextElement.ts
+++ src/qiita/improve-old-article-alert/isAlertTextElement.ts
@@ -1,4 +1,4 @@
 export const isAlertTextElement = (element: Element): boolean => {
   const { textContent } = element
-  return textContent !== null && /^この記事は最終更新日から\d年以上が経過しています。$/.test(textContent)
+  return textContent !== null && /^この記事は最終更新日から\d+年以上が経過しています。$/.test(textContent)
 }
