0149ffb715ac7b2104e146626a627f5cbb4e010e
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-16 20:56:53 +0900

    Create updateAlertTextElement.ts

---
+++ src/qiita/improve-old-article-alert/updateAlertTextElement.ts
@@ -0,0 +1,21 @@
+
+import { differenceInYears } from '../../util/dayjs'
+import { findArticle } from './findArticle'
+
+export const updateAlertTextElement = (alertTextElement: HTMLElement): void => {
+  const article = findArticle()
+
+  if (article === undefined) {
+    throw new Error('Missing article.')
+  }
+
+  const { dateModified } = article
+
+  if (dateModified === undefined) {
+    throw new Error('Missing dateModified.')
+  }
+
+  const now = new Date()
+  const year = differenceInYears(now, dateModified)
+  alertTextElement.textContent = `この記事は最終更新日から${year}年以上が経過しています。`
+}
