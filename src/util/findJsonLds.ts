9c7895d23c9b2dac689df938517d89619450a707
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-16 14:02:18 +0900

    Add comments

--- src/util/findJsonLds.ts
+++ src/util/findJsonLds.ts
@@ -1,3 +1,6 @@
+/**
+ * @see https://json-ld.org/
+ */
 export const findJsonLds = (): string[] => {
   return Array.from(document.querySelectorAll('script[type="application/ld+json"]'))
     .map(({ textContent }) => textContent)
