2fd7a3193f2a4ee24ce8cc0bf6da8ca51f5b91fa
Munieru <20086673+munierujp@users.noreply.github.com>
2023-05-06 17:24:02 +0900

    Update

--- src/tabelog/blocker/main.ts
+++ src/tabelog/blocker/main.ts
@@ -4,10 +4,13 @@ import { RestaurantElement } from './elements'
 import { handleError } from './handleError'
 import { RestaurantHider } from './RestaurantHider'
 
-const db = new Database()
 const restaurantElements = Array.from(document.querySelectorAll<HTMLElement>('.js-rstlist-info .list-rst'))
   .map(element => new RestaurantElement(element))
-const restaurantHider = new RestaurantHider(db)
-restaurantHider.hide(restaurantElements).catch(handleError)
-const blockLinksAppender = new BlockLinksAppender(db)
-blockLinksAppender.append(restaurantElements)
+
+if (restaurantElements.length > 0) {
+  const db = new Database()
+  const restaurantHider = new RestaurantHider(db)
+  restaurantHider.hide(restaurantElements).catch(handleError)
+  const blockLinksAppender = new BlockLinksAppender(db)
+  blockLinksAppender.append(restaurantElements)
+}
