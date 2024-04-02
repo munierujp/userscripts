a79f0d683018211c2bd51156d15988951180f72d
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-03 02:19:12 +0900

    Fix errors

--- src/dmm/books/add-filter-button-on-bookmark-page/Filter.ts
+++ src/dmm/books/add-filter-button-on-bookmark-page/Filter.ts
@@ -5,6 +5,5 @@ export const Filter = {
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Filter = typeof Filter[keyof typeof Filter]
 const values = Object.values(Filter)
-// TODO: fix this error
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 export const isFilter = (value: any): value is Filter => values.includes(value)
