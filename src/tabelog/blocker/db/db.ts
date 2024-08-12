3bb0f8d9950806f938b18a2678b7da48699c2284
Munieru <20086673+munierujp@users.noreply.github.com>
2024-08-13 02:03:17 +0900

    Rename models to model

--- src/tabelog/blocker/db/db.ts
+++ src/tabelog/blocker/db/db.ts
@@ -1,6 +1,6 @@
 import Dexie from 'dexie'
 import type { Table } from 'dexie'
-import type { Restaurant } from './models/Restaurant.js'
+import type { Restaurant } from './model/Restaurant.js'
 
 const DexieClass = Dexie as unknown as typeof Dexie['default']
 
